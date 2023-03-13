fb1067d57effe589bbf70bbda43c8e61df9489ea
PC-LIRAHETA\shenl <le.iraheta@gmail.com>
2023-03-13 17:11:48 -0600

    Files were changed

---
+++ txtFileTest_LOCAL_990.php
@@ -0,0 +1,32 @@
+<?php
+
+use ModbusTcpClient\Network\BinaryStreamConnection;
+use ModbusTcpClient\Packet\ModbusFunction\WriteSingleRegisterRequest;
+use ModbusTcpClient\Packet\ModbusFunction\WriteSingleRegisterResponse;
+use ModbusTcpClient\Packet\ResponseFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$connection = BinaryStreamConnection::getBuilder()
+    ->setPort(502)
+    ->setHost('192.168.100.84')
+    ->build();
+
+$startAddress = 1;
+$value = $_REQUEST['value'];
+$unitID = 0;
+$packet = new WriteSingleRegisterRequest($startAddress, $value * 1000, $unitID); // NB: This is Modbus TCP packet not Modbus RTU over TCP!
+// echo 'Packet to be sent (in hex): ' . $packet->toHex() . PHP_EOL;
+
+try {
+    $binaryData = $connection->connect()
+        ->sendAndReceive($packet);
+    echo '';
+
+} catch (Exception $exception) {
+    echo 'An exception occurred' . PHP_EOL;
+    echo $exception->getMessage() . PHP_EOL;
+    echo $exception->getTraceAsString() . PHP_EOL;
+} finally {
+    $connection->close();
+}
